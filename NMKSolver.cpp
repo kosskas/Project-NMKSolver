d2c1ea3c257d4ac9061b364fbc97e87eea24f5ce
Mateusz Stencel <s188676@o365.student.pg.edu.pl>
2022-05-07 17:12:42 +0200

    stan na 07.05

--- NMKSolver.cpp
+++ NMKSolver.cpp
@@ -16,7 +16,7 @@ enum Position {
 enum State {
     BOTH_PLAYERS_TIE,
     FIRST_PLAYER_WINS,
-    SECOND_PLAYER_WINS
+    SECOND_PLAYER_WINS,
 };
 int** createMap(int n, int m);
 void freeMemory(int** arr, int n);
@@ -25,7 +25,16 @@ void generateAllPositionMoves(int** T, int N, int M, int K, int ActivePlayer);
 int countPosMoves(int** T, int N, int M);
 void generateAllMovesPrintWin(int** T, int N, int M, int K, int ActivePlayer);
 bool checkWinCond(int** T, int N, int M, int K, int ActivePlayer, int y, int x);
+int checkMapIfWin(int** T, int N, int M, int K);
+bool checkHorizontalAxis(int** T, int N, int M, int K, int y, int x);
+bool checkVerticalAxis(int** T, int N, int M, int K, int y, int x);
+bool checkVerticalAxis(int** T, int N, int M, int K,  int y, int x);
+bool checkObliqueAxis(int** T, int N, int M, int K,  int y, int x);
 int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    //skosy
     int N = 0, M = 0, K, player;
     int** T = NULL;
     char command[MAX_CMD_LENGTH];
@@ -41,16 +50,12 @@ int main() {
                 for (int x = 0; x < M; x++) {
                     cin >> T[y][x];
                 }
+            }          
+            if (checkMapIfWin(T, N, M, K) == 0) {
+                generateAllPositionMoves(T, N, M, K, player);
             }
-            
-            for (int y = 0; y < N; y++) {
-                for (int x = 0; x < M; x++) {
-                    if(T[y][x] == player)
-                        checkWinCond(T, N, M, K, player, y, x);
-                }
-            }
-            
-           // generateAllPositionMoves(T, N, M, K, player);
+            else
+                cout << 0 << endl;
 
         }
     }
@@ -97,18 +102,26 @@ int countPosMoves(int** T, int N, int M) {
     return pos_moves;
 }
 
+int checkMapIfWin(int** T, int N, int M, int K) {
+    for (int y = 0; y < N; y++) {
+        for (int x = 0; x < M; x++) {
+            if (checkWinCond(T, N, M, K, FIRST_PLAYER, y, x))
+                return FIRST_PLAYER_WINS;
+            if (checkWinCond(T, N, M, K, SECOND_PLAYER, y, x))
+                return SECOND_PLAYER_WINS;
+        }
+    }
+    return 0;
+}
 void generateAllPositionMoves(int** T, int N, int M, int K, int ActivePlayer) {
-    //sprawdzenie win
     int pos_moves = countPosMoves(T, N, M);
     cout << pos_moves << endl;
-    if (pos_moves > 0) {
-        for (int y = 0; y < N; y++) {
-            for (int x = 0; x < M; x++) {
-                if (T[y][x] == EMPTY) {
-                    T[y][x] = ActivePlayer;
-                    printMap(T, N, M);
-                    T[y][x] = EMPTY;
-                }
+    for (int y = 0; y < N; y++) {
+        for (int x = 0; x < M; x++) {
+            if (T[y][x] == EMPTY) {
+                T[y][x] = ActivePlayer;
+                printMap(T, N, M);
+                T[y][x] = EMPTY;
             }
         }
     }
@@ -130,56 +143,60 @@ void generateAllMovesPrintWin(int** T, int N, int M, int K, int ActivePlayer) {
     }
 }
 
-bool checkWinCond(int** T, int N, int M, int K, int ActivePlayer, int y, int x) {
-    //poziom
-    bool cond = false;
-    cout << "\ndla " << y << " " << x << endl;
-    ///wspolny licznik dla pion
-    cout << "\nPION w dol\n";
-    int k = 0;
-    for (int i = y; i < N; i++) {
-        if (k == K)
-            return true;
-        else if (T[y][x] == T[i][x])
-            k++;
-        else {
-            k = 0;
-            break;
+bool checkHorizontalAxis(int** T, int N, int M, int K,int y, int x) {
+    for (int j = x; j + K - 1 < M; j++) {
+        for (int k = 0; k < K; k++) {
+            if (T[y][j] != T[y][j+k]) {
+                return false;
+            }
         }
-       // cout << T[i][x] << " ";
-    }
-   // cout << "\nPION w gore\n";
-    for (int i = y; i >= 0; i--) {
-        cout << T[i][x] << " ";
-    }
-    ///wspolny licznik dla poziom
-    cout << "\nPOZIOM w prawo\n";
-    for (int j = x; j < M; j++) {
-        cout << T[y][j] << " ";
-    }
-    cout << "\nPOZIOM w lewo\n";
-    for (int j = x; j >= 0; j--) {
-        cout << T[y][j] << " ";
-    }
-    
-    cout <<"\nSKOS W PRAWODOL"<< endl;
-    for (int i = y, j = x; i < N && j < M; i++, j++) {  
-        cout << T[i][j] << " ";
-    }
-    cout <<"\nSKOS W LEWOGORA"<< endl;
-    for (int i = y, j = x; i >=0  && j >= 0; i--, j--) {
-
-        cout << T[i][j] << " ";
-    }
-
-    cout << "\nSKOS W PRAWOGORA" << endl;
-    for (int i = y, j = x; i >=0 && j < M; i--, j++) {
-        cout << T[i][j] << " ";
-    }
-    cout << "\nSKOS W LEWODOL" << endl;
-    for (int i = y, j = x; i < N && j >=0; i++, j--) {
-
-        cout << T[i][j] << " ";
+        return true;
     }
     return false;
+}
+
+bool checkVerticalAxis(int** T, int N, int M, int K, int y, int x) {
+    for (int i = y; i + K - 1 < N; i++) { 
+        for (int k = 0; k < K; k++) {
+            if (T[i][x] != T[i + k][x]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    return false;
+}
+
+bool checkObliqueAxis(int** T, int N, int M, int K, int y, int x) {
+    for (int i = y, j = x; i + K - 1 < N && j + K - 1< M; i++, j++) {
+        for (int k = 0; k < K; k++) {
+            if (T[i][j] != T[i + k][j + k]) {
+                return false;
+            }
+            cout << T[i + k][j + k];
+        }
+        cout << endl;
+
+        return true;
+    }   
+    for (int i = y, j = x; i + K - 1< N && j - K + 1 >= 0; i++, j--) {
+        for (int k = 0; k < K; k++) {
+            if (T[i][j] != T[i + k][j - k]) {
+                //return false;
+            }
+            cout << T[i + k][j - k];
+        }
+        cout << endl;
+        return true;
+    }      
+    return false;
+}
+
+bool checkWinCond(int** T, int N, int M, int K, int ActivePlayer, int y, int x) {
+    if (T[y][x] == ActivePlayer)
+        return checkHorizontalAxis(T, N, M, K, y, x)
+        || checkVerticalAxis(T, N, M, K, y, x)
+        || checkObliqueAxis(T, N, M, K, y, x);
+    else
+        return false;
 }
